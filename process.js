2cb24deb59a08d089f7f7e30ac3b3806d6076a64
Roberto Cannella <robertocannella@gmail.com>
2022-05-11 20:57:58 +0000

    configure firestore methods

--- process.js
+++ process.js
@@ -1,24 +1,27 @@
 import db from "./services/firestore.js";
-import { collection, getDocs, getDoc, onSnapshot, doc } from "firebase/firestore";
+import { collection, getDocs, getDoc, onSnapshot, doc, connectFirestoreEmulator } from "firebase/firestore";
 
 
 console.log('running processes...')
 
 
 let systemID;
-
+let name;
 const systemRef = doc(db, "systems", "MEeFIW6GwQtv1X3Lo7Z2")
-const docSnap = await getDoc(systemRef)
-
+const docSnap = await getDoc(systemRef);
 if (docSnap.exists()) {
 
     //console.log("Document data:", docSnap.data());
+    name = docSnap.data()
+
 } else {
-    // doc.data() will be undefined in this case
+    //doc.data() will be undefined in this case
 
     console.log("No such document!");
 }
-
+console.log(Object.keys(name))
+console.log(name.systemDetails.equipmentId)
+console.log(name.zone12.lineRT[100])
 
 
 
